695aae959db3c204466232b0d956b10fd6ad1e23
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-06 11:18:45 +0700

    week01: initial setup and procedural code

---
+++ oop_00000107437_MuhammadFadhilIlmanKhoir/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week01
+
+fun main() {
+    var name: String = "Jhon Thor"
+    var score: int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
