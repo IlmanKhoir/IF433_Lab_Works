c5dc905c69ae7259be0558591676d55295ada7f6
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-12 05:36:01 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week01/GameStore.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week01/GameStore.kt
@@ -1,6 +1,14 @@
 package oop_00000107437_MuhammadFadhilIlmanKhoir.week01
 
 fun main() {
-    val gameTitle: String = "Ragnarok X"
+    val gameTitle: String = "Elden Ring"
     val price: Int = 750000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000)
+        price - (price * 20 / 100)
+    else
+        price - (price * 10 / 100)
