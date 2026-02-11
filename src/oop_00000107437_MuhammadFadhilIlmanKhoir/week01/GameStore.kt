7c31c63a1c91ab221964b8851be35d45eba19419
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-12 05:47:20 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week01/GameStore.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week01/GameStore.kt
@@ -1,24 +1,33 @@
 package oop_00000107437_MuhammadFadhilIlmanKhoir.week01
 
 fun main() {
-    val gameTitle: String = "Ragnarok X"
+    val gameTitle: String = "Ragnarok x"
     val price: Int = 750000
 
     val finalPrice = calculateDiscount(price)
+    val userNote: String? = null
 
     printReceipt(
         title = gameTitle,
-        finalPrice = finalPrice
+        finalPrice = finalPrice,
+        note = userNote
     )
 }
+
 
 fun calculateDiscount(price: Int): Int =
     if (price > 500000)
         price - (price * 20 / 100)
     else
         price - (price * 10 / 100)
-fun printReceipt(title: String, finalPrice: Int) {
+fun printReceipt(
+    title: String,
+    finalPrice: Int,
+    note: String?
+) {
     println(" penjualan dalam game ")
     println("Game Title : $title")
     println("Final Price: Rp $finalPrice")
+    println("User Note : ${note ?: "Tidak ada catatan"}")
 }
+
