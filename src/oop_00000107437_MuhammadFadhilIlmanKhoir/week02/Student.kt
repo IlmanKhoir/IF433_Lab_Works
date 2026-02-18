d5fc43e4d9ee443a5af73e7a590b92e7693c3553
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-18 12:45:09 +0700

    "week02: implement user input and pre-validation"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week02/Student.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week02/Student.kt
@@ -1,5 +1,31 @@
 package oop_00000107437_MuhammadFadhilIlmanKhoir.week02
 
+import java.util.Scanner
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+    println("===APLIKASI PMB UMN===")
+
+    println("Masukan nama: ")
+    val name = scanner.nextLine()
+
+    println("Masukan NIM (WAJIB 5 KARAKTER): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()//BERSIHKAN BUFFER NEWLINE (PENYAKIT KLASIK SCANNER)
+
+    //VALIDASI DI SISI PEMANGGIL (MAIN)
+    if (nim.length != 5){
+        println("EROR: PENDAFTARAN DIBATALKAN. NIM HARUS 5 KARAKTER!")
+        //PROGRAM BERHASIL DI SINI UNTUK MAHASISWA INI. TIDAK MEMBUAT OBJEK
+    } else{
+        println("MASUKAN JURUSAN: ")
+        val major = scanner.next()
+
+        val s1 = Student(name, nim, major)
+        println("STATUS: PENDAFTARAN SELESAI")
+    }
+}
 class Student (
     val name: String,
     val nim: String,
