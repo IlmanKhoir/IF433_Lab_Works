ad624cda4d4212e26136d229c676325f2f8f7119
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-18 13:52:21 +0700

    "week02: (task 1) implement library input system"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week02/TUGAS1.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week02/TUGAS1.kt
@@ -1,6 +1,35 @@
 package oop_00000107437_MuhammadFadhilIlmanKhoir.week02
 
+import java.util.Scanner
 
+fun main(){
+    val scnanner = Scanner(System.`in`)
+    println("---LIBRARY FINE SIYSTEM")
+
+    println("JUDUL : ")
+    val bookTitle = scnanner.nextLine()
+
+    println("peminjam : ")
+    val borrower = scnanner.nextLine()
+
+    println("Lama Pinjam; ")
+    var loanduration = scnanner.nextInt()
+
+    val loan = Loan (bookTitle, borrower, loanduration)
+
+    val fine = loan.calculateFine()
+
+    if (loanduration < 0){
+        println("WARNING: LAMA PINJAM TIDAK BOLEH MINUS. DIUBAH MENJADI 1 HARI")
+        loanduration = 1
+    }
+
+    println("\n Detail denda Peminjaman")
+    println("Buku: ${loan.bookTitle}")
+    println("Peminjam: ${loan.borrower}")
+    println("Durasi: ${loan.loanduration} hari")
+    println("Denda: Rp $fine")
+}
 
 class Loan(
     val bookTitle: String,
