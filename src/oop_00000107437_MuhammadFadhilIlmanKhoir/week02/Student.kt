a00ccaa54300d31a8725713cebe9eb712b323749
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-18 12:34:14 +0700

    "week02: add warning validation in init block"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week02/Student.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week02/Student.kt
@@ -1,9 +1,17 @@
 package oop_00000107437_MuhammadFadhilIlmanKhoir.week02
 
-class student (
+class Student (
     val name: String,
     val nim: String,
     var major: String,
-){
+)  {
     //Body class kososng dulu
+    init {
+        if (nim.length != 5) {
+            println("WARNING: OBJEK TERCIPTA DENGAN NIM ($nim)")
+            println("DATA MAHASISWA $name MUNGKIN AKAN BERMASALAH DI SISTEM.")
+        } else {
+            println("LOG: OBJEK STUDENT $name BERHASIL DIALOKASIKAN DI MEMORY")
+        }
+    }
 }
