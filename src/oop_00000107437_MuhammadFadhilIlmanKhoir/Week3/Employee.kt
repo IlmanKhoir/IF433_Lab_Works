d82565648a89f1d2bdce187045416a128386cb8f
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-20 11:02:07 +0700

    "week03: test all encapsulation logic in main"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/Week3/Employee.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/Week3/Employee.kt
@@ -4,7 +4,8 @@ package oop_00000107437_MuhammadFadhilIlmanKhoir.Week3
 class Employee(val name : String){
 
     private var performanceRating: Int = 3
-
+    val tax: Double
+        get() = salary *0.1
     var salary: Int = 0
     set(value) {
         if (value < 0){
@@ -27,5 +28,14 @@ class Employee(val name : String){
 fun main(){
     val e = Employee("Budi")
     print("====Test Eror=======")
+    //1. Test validasi Salary
+    e.salary = -1000 // Harus nya print eror
     e.salary = 5000 // ini adak meledakan program anda
+    println("Gaji: ${e.salary}")
+
+    //2. test encapsulation
+    e.increasePerformanceRating()
+
+    //3. Test computed property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
