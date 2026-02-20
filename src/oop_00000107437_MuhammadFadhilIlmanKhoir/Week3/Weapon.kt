d47446b37d46327d8a4702093a65d9495afd85bc
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-20 11:18:17 +0700

    "week03: (task 1) implement Weapon testing in main" (REFACTOR)

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/Week3/Weapon.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/Week3/Weapon.kt
@@ -25,17 +25,3 @@ class weapon(val name: String) {
             }
 }
 
-fun main(){
-    val weapon = weapon("Excalibur")
-
-    // test damage -50 (harus gagal)
-    weapon.damage = -50
-    println("Damage sekarang: ${weapon.damage}")
-
-    // test damage 9999 (harus jadi 1000)
-    weapon.damage = 9999
-    println("Damage sekarang: ${weapon.damage}")
-
-    // print tier
-    println("Tier weapon: ${weapon.tier}")
-}
