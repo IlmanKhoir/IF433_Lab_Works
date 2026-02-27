5d2df0338a8fa77161925640426da7f8d995587f
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-27 15:08:20 +0700

    "week04: (task 2) test Employee hierarchy in main"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week4/Main.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week4/Main.kt
@@ -1,5 +1,8 @@
 package oop_00000107437_MuhammadFadhilIlmanKhoir.week4
 
+import oop_00000107437_MuhammadFadhilIlmanKhoir.week04.Developer
+import oop_00000107437_MuhammadFadhilIlmanKhoir.week04.Manager
+
 fun main(){
     println("---- Testing Vehicle -----")
     val generalVehicle = Vehicle("sepeda ontel")
@@ -18,4 +21,14 @@ fun main(){
     tesla.openTrunck()
     tesla.honk()
     tesla.accelerate()
+
+    println("\n---- Testing Employee Hierarchy ----")
+
+    val manager = Manager("Budi", 10000000)
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
+
+    val developer = Developer("Andi", 8000000, "Kotlin")
+    developer.work()
+    println("Bonus Developer: Rp ${developer.calculateBonus()}")
 }
