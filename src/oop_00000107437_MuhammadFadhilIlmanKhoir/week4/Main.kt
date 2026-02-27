5fb5bf4e55db65529022f71a62bdd622e1ca643d
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-02-27 14:49:26 +0700

    "week04: test Vehicle and Car in main"

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week4/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week4
+
+fun main(){
+    println("---- Testing Vehicle -----")
+    val generalVehicle = Vehicle("sepeda ontel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n ----- Testing Car -----")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunck()
+    myCar.honk()
+    myCar.accelerate()
+}
