4704930b0f0c1c4aa1ba88ccf42024f1edce0d9a
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-03-13 10:11:05 +0700

    "week06: fix diamond problem using explicit super delegation"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/Week06/Smartphone.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/Week06/Smartphone.kt
@@ -2,5 +2,9 @@ package oop_00000107437_MuhammadFadhilIlmanKhoir.Week06
 
 //ERROR: CLASS SMARTPHONE INHERITS MULTIPLE IMPLEMENTATION OF TURNON()
 class Smartphone: camera, phone{
-
+    override fun turnOn() {
+        super<camera>.turnOn() // MENJALANKAN LOGIKA CAMERA
+        super<phone>.turnOn() // MENJALANKAN LOGIKA PHONE
+        println("SISTEM OPERASI SMARTPHONE BERHASIL BOOTING")
+    }
 }
