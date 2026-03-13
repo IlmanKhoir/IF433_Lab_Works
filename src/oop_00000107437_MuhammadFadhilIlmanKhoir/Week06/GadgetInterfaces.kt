4ec2a3f45f01b22d4cddfc2215c28d73d509b15d
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-03-13 10:02:18 +0700

    "week06: create Camera and Phone interfaces with default methods"

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/Week06/GadgetInterfaces.kt
@@ -0,0 +1,12 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.Week06
+
+interface camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+interface phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan")
+    }
+}
