38acab0975c8944f23082bce012c269ef2a7f268
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-04-23 17:46:50 +0700

    create basic UserProfile with nullable fields

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week8/UserProfile.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week8/UserProfile.kt
@@ -1,8 +1,5 @@
 package oop_00000107437_MuhammadFadhilIlmanKhoir.week8
 
-// name adalah Non-Null (wajib), email dan phone adalah Nullable (opsional) [cite: 17]
-package oop_001_johnthor.week08
-
 class UserProfile(
     val name: String,
     val email: String?,
