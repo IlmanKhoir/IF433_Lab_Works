5aa1e6c63c7c81d8a3227e2cdb78aba0de2ced23
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-04-23 18:19:34 +0700

    (task) create Java Payment Service for payment interop

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week8/TugasMandiri/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week8.TugasMandiri;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
