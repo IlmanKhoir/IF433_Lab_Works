5cae7b88fec04e28fed4475248f8a92ea3584f55
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-04-23 18:13:35 +0700

    create LegacyJavaAPI class in Java

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week8/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week8;
+
+public class LegacyJavaAPI {
+    // Simulasi API lama yang tidak memiliki anotasi Nullability
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
