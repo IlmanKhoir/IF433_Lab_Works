dcc19e269aa41220c6d2b3c28b4be8138fd4def1
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-04-23 18:04:46 +0700

    implement safe call with let block

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week8/Main.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week8/Main.kt
@@ -4,4 +4,11 @@ fun main() {
     val emptyOrder = Order(null, null)
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
+
+    val validOrder = Order(null, 250000)
+    val receipt = validOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+    println(receipt)
 }
