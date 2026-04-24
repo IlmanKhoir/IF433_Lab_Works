14952f635384faa19e49d42053baef60b21227e3
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-04-24 10:12:39 +0700

    create TradeLog data class

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week9/TugasMandiri/TradingModels.kt
@@ -0,0 +1,3 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week9.TugasMandiri
+
+data class TradeLog(val pair: String, val position: String, val leverage: Int, val roe: Double,val status: String)
