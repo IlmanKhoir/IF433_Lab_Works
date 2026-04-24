c3b1cce7c092e5f4a7e6820c944f93b208b482cc
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-04-24 10:26:49 +0700

    (task) populate mock trade history list

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week9/TugasMandiri/TradingDashboard.kt
@@ -0,0 +1,19 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week9.TugasMandiri
+
+
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 10, 15.5, "CLOSED"),   // Profit & Closed
+        TradeLog("BTCUSDT", "SHORT", 20, -5.2, "OPEN"),    // Loss & Open
+        TradeLog("ETHUSDT", "LONG", 15, -12.0, "CLOSED"),  // Loss & Closed
+        TradeLog("ETHUSDT", "LONG", 10, 25.0, "OPEN"),     // Profit & Open
+        TradeLog("BTCUSDT", "SHORT", 50, 8.4, "CLOSED"),   // Profit & Closed
+        TradeLog("ETHUSDT", "SHORT", 5, -2.5, "CLOSED")    // Loss & Closed
+    )
+
+    println(" === Riwayat Trading ===")
+    tradeHistory.forEach { log ->
+        println("Pair : ${log.pair} | ROE : ${log.roe}% | Status : ${log.status}")
+    }
+}
