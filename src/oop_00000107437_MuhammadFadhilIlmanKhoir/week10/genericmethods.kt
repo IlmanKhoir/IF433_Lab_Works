791a89fb0971682c2fbb50c6c67a9e48f09535df
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-05-08 08:25:29 +0700

    "week10: implement generic function with return type"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week10/genericmethods.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week10/genericmethods.kt
@@ -3,3 +3,6 @@ package oop_00000107437_MuhammadFadhilIlmanKhoir.week10
 fun <T> printdata(data: T) {
     println("data yang terima : $data")
 }
+fun <T> processdata(input: T) :  T{
+    return input
+}
