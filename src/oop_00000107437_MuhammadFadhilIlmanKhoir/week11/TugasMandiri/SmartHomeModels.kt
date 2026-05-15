f9294d78a245b0e7c13fd6d797de38410eaac691
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-05-15 08:23:14 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week11/TugasMandiri/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week11.TugasMandiri
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
