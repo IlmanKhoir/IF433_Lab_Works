cef8c3b58ee5e86508791ccbc4657f8a9ecaf1a1
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-05-15 08:07:08 +0700

    week10: (task) populate coin repository

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week10/tugasmandiri/CryptoDashboard.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week10/tugasmandiri/CryptoDashboard.kt
@@ -3,5 +3,7 @@ package oop_00000107437_MuhammadFadhilIlmanKhoir.week10.tugasmandiri
 fun main() {
     val coinRepo = WalletRepository<coin>()
 
-
+    coinRepo.add(coin("BTC", 0.5))
+    coinRepo.add(coin("ETH", 10.0))
+    coinRepo.add(coin("USDT", 2500.0))
 }
