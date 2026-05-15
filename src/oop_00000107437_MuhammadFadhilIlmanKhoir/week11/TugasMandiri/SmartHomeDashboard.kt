e41385d0385c106c3d618730cdf0c2601970a668
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-05-15 08:26:21 +0700

    week11: (task) initialize smart home device list

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week11/TugasMandiri/SmartHomeDashboard.kt
@@ -0,0 +1,6 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week11.TugasMandiri
+
+fun main() {
+    // CHECKPOINT 13: Siapkan penampung perangkat
+    val homeDevices = mutableListOf<SmartDevice>()
+}
