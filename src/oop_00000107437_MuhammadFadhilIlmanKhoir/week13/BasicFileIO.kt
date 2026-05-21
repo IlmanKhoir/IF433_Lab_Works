a3089e522a9170652b002215f4b5918a8a8afd6c
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-05-21 21:02:29 +0700

    "week13: implement basic writeText function"

---
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week13/BasicFileIO.kt
@@ -0,0 +1,10 @@
+package oop_00000107437_MuhammadFadhilIlmanKhoir.week13
+
+fun main() {
+    // --- CHECKPOINT 1 ---
+    println("=== TEST WRITE TEXT ===")
+    val file = File("notes.txt")
+    file.writeText("Line 1: Inisialisasi sistem.\n")
+    println("File berhasil dibuat dan ditulis.")
+
+}
