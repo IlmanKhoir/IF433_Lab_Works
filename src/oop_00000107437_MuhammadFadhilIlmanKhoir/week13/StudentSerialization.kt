57caccd280d9b3c900619afc002f90be2c36d1c6
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-05-21 21:13:06 +0700

    "week13: create save and load list functions"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week13/StudentSerialization.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week13/StudentSerialization.kt
@@ -6,3 +6,17 @@ data class Student(val name: String, val age: Int, val gpa: Double)
 // --- CHECKPOINT 8 ---
 // Serialization (Object -> CSV)
 fun Student.toCsv(): String = "$name,$age,$gpa"
+
+// --- CHECKPOINT 9 ---
+fun saveStudents(students: List<Student>, path: String) {
+    File(path).writeText(students.joinToString(separator = "\n") { it.toCsv() })
+}
+
+fun loadStudents(path: String): List<Student> {
+    return try {
+        File(path).readLines().map { fromCsv(it) }
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
+    }
+}
