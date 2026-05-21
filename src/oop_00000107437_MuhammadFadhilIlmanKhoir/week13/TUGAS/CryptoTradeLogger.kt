48bd355fb72ecb2c1ad144dae0633ef8c561395b
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-05-21 21:18:42 +0700

    "week13: (task) implement fromCsvTrade with split" & "week13: (task) wrap fromCsvTrade in try-catch for NumberFormatException"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week13/TUGAS/CryptoTradeLogger.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week13/TUGAS/CryptoTradeLogger.kt
@@ -14,3 +14,22 @@ data class TradeRecord(
 // --- CHECKPOINT 12 ---
 // Serialization
 fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
+
+// --- CHECKPOINT 13 & 14 ---
+// Deserialization dengan pengaman error handling
+fun fromCsvTrade(line: String): TradeRecord? {
+    return try {
+        val parts = line.split(",")
+        TradeRecord(
+            id = parts[0].trim().toInt(),
+            symbol = parts[1].trim(),
+            type = parts[2].trim(),
+            margin = parts[3].trim().toDouble(),
+            pnl = parts[4].trim().toDouble()
+        )
+    } catch (e: Exception) {
+        // Menangkap NumberFormatException, IndexOutOfBoundsException, dll.
+        println("(Log) Data korup diabaikan: $line")
+        null
+    }
+}
