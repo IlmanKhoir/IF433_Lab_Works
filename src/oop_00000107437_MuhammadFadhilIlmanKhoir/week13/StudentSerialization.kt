23e6d9aa94a00d8e66b61a8b675667acdd6efe55
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2026-05-21 21:12:18 +0700

    "week13: implement toCsv and fromCsv logic"

--- src/oop_00000107437_MuhammadFadhilIlmanKhoir/week13/StudentSerialization.kt
+++ src/oop_00000107437_MuhammadFadhilIlmanKhoir/week13/StudentSerialization.kt
@@ -2,3 +2,7 @@ package oop_00000107437_MuhammadFadhilIlmanKhoir.week13
 
 // --- CHECKPOINT 7 ---
 data class Student(val name: String, val age: Int, val gpa: Double)
+
+// --- CHECKPOINT 8 ---
+// Serialization (Object -> CSV)
+fun Student.toCsv(): String = "$name,$age,$gpa"
